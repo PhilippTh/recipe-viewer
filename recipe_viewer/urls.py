3501872f5c40a709f7edbcd14c6673d1cd6a93da
Philipp Thumfart <philipp.thumfart@freshfields.com>
2025-11-13 15:37:09 +0100

    Added ingredient adding field

--- recipe_viewer/urls.py
+++ recipe_viewer/urls.py
@@ -23,6 +23,7 @@ from django.urls import path
 from recipe_viewer.apps.recipes.views import RecipeChangeView
 from recipe_viewer.apps.recipes.views import RecipeCreateView
 from recipe_viewer.apps.recipes.views import RecipeDetailView
+from recipe_viewer.apps.recipes.views import add_ingredient_form
 from recipe_viewer.apps.recipes.views import recipe_ingredients
 from recipe_viewer.apps.recipes.views import recipe_list
 from recipe_viewer.apps.recipes.views import set_language
@@ -31,6 +32,7 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", recipe_list, name="recipe_list"),
     path("recipe/create/", RecipeCreateView.as_view(), name="recipe_create"),
+    path("recipe/create/add-ingredient-form/", add_ingredient_form, name="add_ingredient_form"),
     path("recipe/<int:recipe_id>/", RecipeDetailView.as_view(), name="recipe_detail"),
     path("recipe/<int:recipe_id>/change/", RecipeChangeView.as_view(), name="recipe_change"),
     path("recipe/<int:recipe_id>/ingredients/", recipe_ingredients, name="recipe_ingredients"), # type: ignore[arg-type]
